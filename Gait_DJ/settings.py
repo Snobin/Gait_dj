e6774b573659f4cc7b66c40b508b90a50d25d582
Snobin <47213116+Snobin@users.noreply.github.com>
2023-05-05 11:01:14 +0530

    Update settings.py

--- Gait_DJ/settings.py
+++ Gait_DJ/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iw7zqq5g5+w^ew&3p#r0ds-i%8#$c*k*ushvh-h*ecdl!hlw!b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
